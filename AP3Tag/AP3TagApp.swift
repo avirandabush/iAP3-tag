b40e29386fe5ca8be5c5e6c75dd8d83bc33a5a8e
Aviran Dabush <avirandabush@gmail.com>
2025-11-25 17:36:35 +0200

    init project.

---
+++ AP3Tag/AP3TagApp.swift
@@ -0,0 +1,17 @@
+//
+//  AP3TagApp.swift
+//  AP3Tag
+//
+//  Created by Aviran Dabush on 25/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct AP3TagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
